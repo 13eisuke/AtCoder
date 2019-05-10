5962bed93f27f6de7205fa3d878c391ccf3b7398
eisuke <ei13suke@gmail.com>
2019-05-10 23:34:43 +0900

    done

--- ABC105/abc105c.cpp
+++ ABC105/abc105c.cpp
@@ -9,11 +9,24 @@ using namespace std;
 int main()
 {
     int n;
+    string s;
 
     cin >> n;
-
-    for(int i=0; ;i++){
-        
+    if (n == 0) {
+        cout << 0 << endl;
+        return 0;
     }
+
+    while (n) {
+        if (n % 2) {
+            s += '1';
+            n -= 1;
+        } else {
+            s += '0';
+        }
+        n /= -2;
+    }
+    reverse(s.begin(), s.end());
+    cout << s << endl;
     return 0;
 }
