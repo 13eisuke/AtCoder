2e4d0dd1b29db8d1f7805dcbdd2c383eabb2399e
wt-l00 <ei13suke@gmail.com>
2019-08-29 08:30:43 +0900

    done

---
+++ ABC134/abc134e.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+ll MOD = 1000000007;
+
+typedef long long ll;
+
+int main()
+{
+    int n;
+    cin >> n;
+    vector <int> a(n, 0);
+    for (int i = 0; i < n; i++) cin >> a[i];
+    return 0;
+}
+
+
---
+++ AGC035/agc035a.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+ll MOD = 1000000007;
+
+int main()
+{
+    int N;
+    cin >> N;
+    vector <int> a(N, 0);
+    for (int i = 0; i < N; i++) cin >> a[i];
+    
+    return 0;
+}
