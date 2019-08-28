0bf4dc3b4fef007213c5d3cae71a720a4a4e9cc1
wt-l00 <ei13suke@gmail.com>
2019-08-29 08:30:31 +0900

    done

--- ABC135/abc135d.cpp
+++ ABC135/abc135d.cpp
@@ -1,16 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
-
 typedef long long ll;
 ll MOD = 1000000007;
 
-typedef long long ll;
-// dp[i][j] := 先頭 i 文字として考えられるもののうち，13 で割ったあまりが j であるものの数
-int main()
+int dp[100001][13];
+
+signed main()
 {
     string s;
     cin >> s;
-    int dp[100005][13];
+    
     int n = s.size();
 
     dp[0][0] = 1;
@@ -24,13 +23,6 @@ int main()
             }
         }
     }
-    /*
-    for (int i = 0; i <= n; i++) {
-        for (int j = 0; j < 13; j++) {
-            cout << dp[i][j] << " ";
-        }
-        cout << endl;
-    }*/
     cout << dp[n][5] << endl;
 
     return 0;
