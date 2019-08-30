bc7aeb61d15444364ae935278cc62fa37aa8baeb
wt-l00 <ei13suke@gmail.com>
2019-08-30 15:30:00 +0900

    done

--- ABC134/abc134e.cpp
+++ ABC134/abc134e.cpp
@@ -2,17 +2,23 @@
 using namespace std;
 
 typedef long long ll;
-ll MOD = 1000000007;
-
-typedef long long ll;
+const ll MOD = 1e9 + 7;
+const ll inf = 1LL<<60;
+const double eps = 1e-9;
 
 int main()
 {
-    int n;
-    cin >> n;
-    vector <int> a(n, 0);
-    for (int i = 0; i < n; i++) cin >> a[i];
+    int N;
+    cin >> N;
+    multiset <int> st;
+
+    for (int i = 0; i < N; i++) {
+        int x;
+        cin >> x;
+        auto k = st.lower_bound(x);
+        if (k != st.begin()) st.erase(--k);
+        st.insert(x);
+    }
+    cout << st.size() << endl;
     return 0;
 }
-
-
