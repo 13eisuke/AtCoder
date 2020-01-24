a2b1bed3ffbdc8bb366ba65f28110bae4f1edca6
wt-l00 <ei13suke@gmail.com>
2020-01-24 20:15:28 +0900

    done

--- temp.cpp
+++ temp.cpp
@@ -13,3 +13,17 @@ int main()
     return 0;
 }
 
+ll gcd(ll a, ll b)
+{
+    if (a < b) {
+        a ^= b;
+        b ^= a;
+        a ^= b;
+    }
+    return b ? gcd(b, a % b) : a;
+};
+
+ll lcm(ll a, ll b)
+{
+    return a * b / gcd(a, b);
+};
