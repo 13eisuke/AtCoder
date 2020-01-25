ee7ceafcffce5171eb953d6f92b96511b5a1a52e
wt-l00 <ei13suke@gmail.com>
2020-01-25 16:25:18 +0900

    done

--- ABC152/abc152e.cpp
+++ ABC152/abc152e.cpp
@@ -1,46 +1,57 @@
+
 #include <bits/stdc++.h>
 using namespace std;
 
 typedef long long ll;
-const ll MOD = 1e9 + 7;
+const ll mod = 1e9 + 7;
 const ll inf = 1LL<<60;
 const double eps = 1e-9;
 
 typedef pair<ll, ll> P;
-ll gcd(ll a, ll b)
-{
-    if (a < b) {
-        a ^= b;
-        b ^= a;
-        a ^= b;
-    }
-    
-    return b ? gcd(b, a % b) : a;
-};
+template<class T>bool chmax(T &a,T b){if(a<b){a=b;return 1;}return 0;}
+template<class T>bool chmin(T &a,T b){if(a>b){a=b;return 1;}return 0;}
 
-ll lcm(ll a, ll b)
+ll mpow(ll a, ll b)
 {
-    return a * b / gcd(a, b);
-};
+  if(b == 0) return 1; 
+  return mpow(a*a%mod, b/2)*(b%2?a:1)%mod;
+}
 
 int main()
 {
-    ll n, x, sum = 0;
+    ll n;
     cin >> n;
     vector <ll> a(n);
-    ll tmp;
-    cin >> tmp;
-    for (int i = 1; i < n; i++) {
-        cin >> x;
-        tmp = lcm(tmp, x);
+    ll p[1000010] = {0};
+    for (ll i = 0; i < n; i++) cin >> a[i];
+    for (ll i = 0; i < n; i++) {
+        ll tmp = a[i];
+        for (ll j = 2; j*j <= tmp; j++) {
+            ll cnt = 0;
+            while (tmp % j == 0) {
+                cnt++;
+                tmp /= j;
+            }
+            chmax(p[j], cnt);
+        }
+        if (tmp) chmax(p[tmp], 1LL);
     }
-    tmp %= MOD;
-    cout << tmp << endl;
-    for (int i = 0; i < n; i++) {
-        cout << sum << endl;
-        sum += (tmp%MOD/a[i]%MOD);
+
+    ll tmp = 1;
+    for (ll i = 0; i < 1000010; i++) {
+        for (ll j = 0; j < p[i]; j++) {
+            //cout << i << endl;
+            (tmp *= i) %= mod;
+        }
     }
-    cout << sum << endl;
+    //cout << tmp << endl;
+
+    ll ans = 0;
+    for (ll i = 0; i < n; i++) {
+        //(ans += tmp*mpow(a[i], mod-2)%mod) %= mod;
+        (ans += tmp*mpow(a[i], mod-2)%mod) %= mod;
+    }
+    cout << ans << endl;
     return 0;
 }
 
