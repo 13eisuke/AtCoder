3e78398047191baa48bbed97f1961ee5fa95f5be
wt-l00 <ei13suke@gmail.com>
2020-02-13 18:58:30 +0900

    done

---
+++ ABC056/abc056c.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+const ll MOD = 1e9 + 7;
+const ll inf = 1LL<<60;
+const double eps = 1e-9;
+
+typedef pair<ll, ll> P;
+const int dx[4] = { 1, 0, -1, 0 };
+const int dy[4] = { 0, 1, 0, -1 };
+ 
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+
+int main()
+{
+    ll n;
+    cin >> n;
+    
+    for (ll i = 1; i <= n; i++) {
+        if (i*(i+1)/2 >= n) {
+            cout << i << endl;
+            break;
+        }
+    }
+    return 0;
+}
+
