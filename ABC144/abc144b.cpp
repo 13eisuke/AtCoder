ebe5efeaa2434cca784be1e8147b6068d091397b
wt-l00 <ei13suke@gmail.com>
2020-02-22 20:24:06 +0900

    done

--- ABC144/abc144b.cpp
+++ ABC144/abc144b.cpp
@@ -9,16 +9,19 @@ const ll MOD = 1e9 + 7;
 const ll inf = 1LL<<60;
 const double eps = 1e-9;
 
-
-
 int main()
 {
-    int a, b;
-    cin >> a >> b;
-    if (a <= 9 and b <= 9) cout << a*b << endl;
-    else cout << -1 << endl;
+    int n;
+    cin >> n;
+
+    for (int i = 1; i <= 9; i++) {
+        for (int j = 1; j <= 9; j++) {
+            if (n == i*j) {
+                cout << "Yes" << endl;
+                return 0;
+            }
+        }
+    }
+    cout << "No" << endl;
     return 0;
 }
-
-
-
