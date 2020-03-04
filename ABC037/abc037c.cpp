181af58323b8df9e9923b41ed7050dec75bf5622
wt-l00 <ei13suke@gmail.com>
2020-03-04 19:54:57 +0900

    done

---
+++ ABC037/abc037c.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef pair<ll, ll> P;
+
+const ll MOD = 1e9 + 7;
+const ll inf = 1LL<<60;
+const double eps = 1e-9;
+
+const int dx[4] = { 1, 0, -1, 0 };
+const int dy[4] = { 0, 1, 0, -1 };
+ 
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+
+int main()
+{
+    ll n, k;
+    cin >> n >> k;
+    ll now = 0, ans = 0;
+    vector <int> a(n);
+
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+        if (i < k) now += a[i];
+    }
+
+    ans += now;
+    
+    for (int i = 0; i < n; i++) {
+        if (i < k) continue;
+        else {
+            now -= a[i-k];
+            now += a[i];
+            ans += now;
+        }
+    }
+    cout << ans << endl;
+
+    return 0;
+}
+
