43f007aebf0b02dd313de508a4657772e9092d9d
wt-l00 <ei13suke@gmail.com>
2020-04-20 07:32:37 +0900

    Add ABC163

---
+++ ABC163/abc163c.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef pair<ll, ll> P;
+
+const ll MOD = 1e9 + 7;
+const ll inf = 1LL<<60;
+const int int_inf = 100000000;
+const double eps = 1e-9;
+
+const int dx[4] = { 1, 0, -1, 0 };
+const int dy[4] = { 0, 1, 0, -1 };
+ 
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+
+
+int main()
+{
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+    
+    int n;
+    cin >> n;
+
+    vector<int> a(n, 0);
+
+    for (int i = 0; i < n-1; i++) {
+        int tmp;
+        cin >> tmp;
+        a[tmp-1]++;
+    }
+
+    for (int i = 0; i < n; i++) cout << a[i] << endl;
+    return 0;
+}
+
